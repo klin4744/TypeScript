08b3663f1e5a0834b913ec9c1ef677bfb6f9dfa8
Kevin Lin <kevin.lin.2@stonybrook.edu>
2019-09-11 01:32:14 -0400

    Add win check

--- app.js
+++ app.js
@@ -7,15 +7,52 @@ var Move;
 })(Move || (Move = {}));
 var TickTacToe = /** @class */ (function () {
     function TickTacToe() {
-        this.won = false;
+        this.won = [false, ''];
         this.board = [
             [Move.Empty, Move.Empty, Move.Empty],
             [Move.Empty, Move.Empty, Move.Empty],
             [Move.Empty, Move.Empty, Move.Empty],
         ];
+        this.currentPlayer = Move.X;
     }
-    TickTacToe.getBoard = function () {
+    TickTacToe.prototype.getBoard = function () {
         return this.board;
+    };
+    TickTacToe.prototype.getGameState = function () {
+        if (this.won[0]) {
+            return "The " + this.won[1] + " player has won!";
+        }
+        return "No one has won yet";
+    };
+    TickTacToe.prototype.makeMove = function (player, position) {
+        try {
+            if (this.board[position[0]][position[1]])
+                throw new Error('That spot is occupied! Please Make a different move');
+            this.board[position[0]][position[1]] === player;
+            if (this.checkWon()) {
+                this.won[1] = player === Move.X ? 'X' : 'O';
+                this.getGameState();
+            }
+            console.log("Move made! by " + player);
+            player === Move.X
+                ? (this.currentPlayer = Move.O)
+                : (this.currentPlayer = Move.X);
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    };
+    TickTacToe.prototype.checkWon = function () {
+        var arrStr = this.board.join('\n').replace(/,/g, '');
+        var winRegex = /1{3}|2{3}|1.{2}\n.1.\n.{2}1|2.{2}\n.2.\n.{2}2|1.{2}\n1.{2}\n1.{2}|2.{2}\n2.{2}\n2.{2}|.1.\n.1.\n.1.|.2.\n.2.\n.2.|.{2}1\n.{2}1\n.{2}1|.{2}2\n.{2}2\n.{2}2/;
+        var matches = arrStr.match(winRegex);
+        if (matches) {
+            this.won[0] = true;
+        }
+        return true;
     };
     return TickTacToe;
 }());
+var game = new TickTacToe();
+console.log(game.getBoard());
+console.log(game.checkWon());
--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ enum Move {
 class TickTacToe {
    private won: [boolean, string];
    private board: Move[][];
+   private currentPlayer: Move;
    constructor() {
       this.won = [false, ''];
       this.board = [
@@ -13,6 +14,7 @@ class TickTacToe {
          [Move.Empty, Move.Empty, Move.Empty],
          [Move.Empty, Move.Empty, Move.Empty],
       ];
+      this.currentPlayer = Move.X;
    }
    public getBoard(): Move[][] {
       return this.board;
@@ -23,7 +25,36 @@ class TickTacToe {
       }
       return `No one has won yet`;
    }
-   public makeMove(player: Move, position: [number, number]) {
-      this.board[position[0]][position[1]] === player;
+   public makeMove(player: Move, position: [number, number]): void {
+      try {
+         if (this.board[position[0]][position[1]])
+            throw new Error(
+               'That spot is occupied! Please Make a different move',
+            );
+         this.board[position[0]][position[1]] === player;
+         if (this.checkWon()) {
+            this.won[1] = player === Move.X ? 'X' : 'O';
+            this.getGameState();
+         }
+         console.log(`Move made! by ${player}`);
+         player === Move.X
+            ? (this.currentPlayer = Move.O)
+            : (this.currentPlayer = Move.X);
+      } catch (error) {
+         console.log(error.message);
+      }
+   }
+   public checkWon(): boolean {
+      let arrStr: string = this.board.join('\n').replace(/,/g, '');
+      let winRegex = /1{3}|2{3}|1.{2}\n.1.\n.{2}1|2.{2}\n.2.\n.{2}2|1.{2}\n1.{2}\n1.{2}|2.{2}\n2.{2}\n2.{2}|.1.\n.1.\n.1.|.2.\n.2.\n.2.|.{2}1\n.{2}1\n.{2}1|.{2}2\n.{2}2\n.{2}2/;
+      let matches: any = arrStr.match(winRegex);
+      if (matches) {
+         this.won[0] = true;
+      }
+      return true;
    }
 }
+
+const game: TickTacToe = new TickTacToe();
+console.log(game.getBoard());
+console.log(game.checkWon());
